77eabf9fd73c6471fadd9cc101873635481c04c1
Nicholas Browning <systems@telegeography.com>
2013-09-04 10:28:37 -0400

    Adding Netnod-STH

--- public/public/api/buildings/8100.js
+++ public/public/api/buildings/8100.js
@@ -1,1 +1,1 @@
-{"address":["\u003Caddress not available\u003E","Philadelphia, United States"],"id":8100,"exchanges":[{"slug":"phiix-philadelphia-united-states","name":"PhIIX","info":[{"onclick":"window.open(this.href,'ix-new-window');return false;","link":"http://www.phlix.net","value":"Website"}]}]}
+{"exchanges":[{"slug":"phiix-philadelphia-united-states","name":"PhIIX","info":[{"onclick":"window.open(this.href,'ix-new-window');return false;","link":"http://www.phlix.net","value":"Website"}]}],"address":["\u003Caddress not available\u003E","Philadelphia, United States"],"id":8100}
