5764430c23e9bd407fda750404931989fb1e7158
Nicholas Browning <systems@telegeography.com>
2013-09-06 14:00:25 -0400

    CATNIX info updated

--- public/public/api/buildings/8100.js
+++ public/public/api/buildings/8100.js
@@ -1,1 +1,1 @@
-{"exchanges":[{"slug":"phiix-philadelphia-united-states","name":"PhIIX","info":[{"onclick":"window.open(this.href,'ix-new-window');return false;","link":"http://www.phlix.net","value":"Website"}]}],"address":["\u003Caddress not available\u003E","Philadelphia, United States"],"id":8100}
+{"address":["\u003Caddress not available\u003E","Philadelphia, United States"],"id":8100,"exchanges":[{"info":[{"onclick":"window.open(this.href,'ix-new-window');return false;","link":"http://www.phlix.net","value":"Website"}],"slug":"phiix-philadelphia-united-states","name":"PhIIX"}]}
