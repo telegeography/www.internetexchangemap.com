a32d364739c7452720474b2c3c3180936748edb4
Nicholas Browning <systems@telegeography.com>
2014-02-25 13:01:03 -0500

    Updated contact info and some location info for the following: EKT-IX, KZN-IX, MSK-IX, NSK-IX, RND-IX, SMR-IX, STW-IX, SPB-IX, and VLV-IX

--- production/api/v1/buildings/18473.js
+++ production/api/v1/buildings/18473.js
@@ -1,1 +1,1 @@
-{"exchanges":[{"slug":"linx-nova-washington-united-states","name":"LINX NoVA","info":[]}],"address":["9651 Hornbaker Road","Manassas, United States, 20109"],"id":18473}
+{"address":["9651 Hornbaker Road","Manassas, United States, 20109"],"exchanges":[{"slug":"linx-nova-washington-united-states","name":"LINX NoVA","info":[]}],"id":18473}
