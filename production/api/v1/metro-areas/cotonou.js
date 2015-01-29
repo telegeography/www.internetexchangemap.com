3c535822007407e1b8fcf36b9b42dbbe40f70efb
Nicholas Browning <systems@telegeography.com>
2015-01-29 13:59:25 -0500

    Added Benin-IX

---
+++ production/api/v1/metro-areas/cotonou.js
@@ -0,0 +1,1 @@
+{"buildings":[{"latitude":"6.356879","longitude":"2.44011","address":["\u003Caddress not available\u003E","Cotonou, Benin"],"offset":"background:url('images/markers.png') no-repeat -1166px 0;","id":19635}],"name":"Cotonou","id":"cotonou"}
